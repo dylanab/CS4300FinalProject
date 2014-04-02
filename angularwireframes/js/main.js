27a820d4dfba459a81bb16455c4d850bebd3a917
Archos67 <ghyzel.chris@gmail.com>
2014-04-02 15:18:46 -0400

    added sample search functionality TODO: refine

---
+++ angularwireframes/js/main.js
@@ -0,0 +1,40 @@
+var app = angular.module('articleApp', []);
+
+app.factory('ArticleList', function() {
+    var articleList = {};
+    articleList.articles = [ 
+	{
+	    title: "Malaysia Airlines",
+	    author: "CNN",
+	    categories: ["Ratings Cow", "Tragedy"],
+	    date: "March 19, 2014",
+	    hits: 1000
+	},
+	{
+	    title: "Cat is cute",
+	    author: "kitty",
+	    categories: ["Cute"],
+	    date: "March 11, 2014",
+	    hits: 100000
+	},
+	{
+	    title: "Obamacare obamacare obamacare",
+	    author: "Amerikan Konservative",
+	    categories: ["Politics"],
+	    date: "All day e'veryday",
+	    hits: 500
+	},
+	{
+	    title: "Crimea Invasion",
+	    author: "BBC",
+	    categories: ["Politics","Russia Scary"],
+	    date: "March 6, 2014",
+	    hits: 2000
+	}
+    ];
+    return articleList;
+});
+function ArticleCtrl($scope, ArticleList) {
+    $scope.articleList = ArticleList;
+};
+//function ArticleCtrl2($scope, ArticleL
