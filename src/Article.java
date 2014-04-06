deb7630bdaf2f1dcb7da5b92587371bb39fcdabb
luke-grantham <lgrantham2@gmail.com>
2014-04-06 16:39:19 -0400

    Added User DTO and edited Article DTO

--- src/Article.java
+++ src/Article.java
@@ -12,14 +12,20 @@ private String title;
 private String text;
 private String image_path;
 private String categories;
+private int hits;
+private int author_id;
+private int response_id;
 
-Article(int id, User user, String title, String text, String image_path, String categories){
+Article(int id, User user, String title, String text, String image_path, String categories, int hits, int author_id, int response_id){
 	this.id = id;
 	this.user = user;
 	this.title = title;
 	this.text = text;
 	this.image_path = image_path;
 	this.categories = categories;
+	this.hits = hits;
+	this.author_id = author_id;
+	this.response_id = response_id;
 } //constructor
 
 
@@ -30,7 +36,9 @@ public String getTitle(){ return title; }
 public String getText(){ return text; }
 public String getImage_path(){ return image_path; }
 public String getCategories(){ return categories; }
-
+public int getHits(){ return hits; }
+public int getAuthor_id(){ return author_id; }
+public int getResponse_id(){ return response_id; }
 
 
 } //categories
