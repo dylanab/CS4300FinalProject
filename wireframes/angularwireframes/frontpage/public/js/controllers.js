fd2e530a064a5e9c2b9a740ed0841a9d25f53575
Archos67 <ghyzel.chris@gmail.com>
2014-04-13 18:13:25 -0400

    Added dynamic main article to controller logic

--- wireframes/angularwireframes/frontpage/public/js/controllers.js
+++ wireframes/angularwireframes/frontpage/public/js/controllers.js
@@ -1,18 +1,31 @@
-function HomeCtrl($scope, ArticleList) {
-    console.log(ArticleList);
+function SearchCtrl($scope, $routeParams, filterFilter, orderByFilter, ArticleList) {
     $scope.articleList = ArticleList;
     $scope.filter = "$";
-    $scope.search = {name:'', categories:'', $:''};
-    $scope.changeCategoryTo = function(property) {
-	$scope.search.categories = property;
-    }
-}
-function SearchCtrl($scope, ArticleList) {
-    $scope.articleList = ArticleList;
-    $scope.filter = "$";
-    $scope.search = {name:'', categories:'', $:''};
-    $scope.changeCategoryTo = function(property) {
-	$scope.search.categories = property;
-    }
 
+    $scope.search = {name:'', categories:'', $:''};
+    $scope.searchBox = '';
+    $scope.mainArticleId = {};
+    $scope.changeCategoryTo = function(property) {
+	$scope.search.categories = property;
+	$scope.search.name = $scope.searchBox = '';
+
+	if(!$routeParams.articleId) {
+	    $scope.mainArticleId = filterFilter(orderByFilter($scope.articleList.articles, "-hits"), $scope.search.categories)[0].title;
+	} else {
+	    $scope.mainArticleId = $routeParams.articleId;
+	}
+    }
+    if(!$routeParams.articleId) {
+	$scope.mainArticleId = filterFilter(orderByFilter($scope.articleList.articles, "-hits"), $scope.search.categories)[0].title;
+
+    } else {
+	$scope.mainArticleId = $routeParams.articleId;
+    }
+    $scope.updateSearch = function () {
+	$scope.search.name = $scope.searchBox;
+
+    }
+    $scope.changeMainArticle = function(title) {
+	ArticleList
+    }
 }
