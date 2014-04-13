88a0a27d386f3c5c29045689c31d1c96db942d7a
Dylan <dylanab@uga.edu>
2014-04-13 16:02:10 -0400

    Added login controller

--- src/LoginController.java
+++ src/LoginController.java
@@ -25,30 +25,45 @@ public class LoginController extends HttpServlet {
 	 * Idempotent actions: 
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-	
-	}
-
-
-
+	    try {
+		ServletContext ctx = this.getServletContext();
+		RequestDispatcher dispatcher;
+	    } catch(Exception e) {
+		e.getMessage();
+	    }
+	}//doGet
 
 	/**
 	 * Non-Idempotent actions: Login
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-    try{
-	ServletContext ctx = this.getServletContext();
-	RequestDispatcher dispatcher;
+	    try{
+		ServletContext ctx = this.getServletContext();
+		RequestDispatcher dispatcher;
+	    }//try
+	    catch(Exception e){
+		e.getMessage();
+	    }
+	    String username = request.getParameter("username");
+	    String password = request.getParameter("password");
 
+	    String hex_pass = Encrypyer.toSha256(password);
 
-
-
-
-    }//try
-    catch(Exception e){
-	e.getMessage();
-    }
- }//doPost
-
-
-
+	    UserHelper userhelper = new UserHelper();
+	    User user = userhelper.validate(username, hex_pass);
+	    if(user != null) {
+		//login successful.
+		//store the user info in the session.
+		request.getSession().setAttribute("id", user.getId());
+		request.getSession().setAttribute("username", user.getName());
+		request.getSession().setAttribute("role", user.getRole());
+		//route to profile, might need to change this to a different jsp.
+		dispatcher = ctx.getRequestDispatcher("/profile.jsp");
+	    } else {
+		//login unsuccessful, route back to login with a thing to set off the c:if
+		request.setAttribute("error", "name/pass incorrect");
+		dispatcher = ctx.getReqestDispatcher("/login.jsp");
+	    }
+	    
+	}//doPost
 }//class
