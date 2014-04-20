b8f5015c145272c7c37982940db577a5bc13a839
mtank <mtankersley101@gmail.com>
2014-04-20 16:09:03 -0400

    test db schema
    added a schema for a test database; don't worry to much, it just creates
    a blank copy of the production database

---
+++ db/test_schema_for_project.sql
@@ -0,0 +1,30 @@
+#
+#	this SQL file creates the schema for the Politalk database
+#
+
+use testpolitalk;
+
+DROP TABLE IF EXISTS Users;
+
+CREATE TABLE Users (
+	Uid INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+	Username VARCHAR(50),
+	Password VARCHAR(64),
+	ProfilePicPath VARCHAR(200),
+	Role INT
+);
+
+DROP TABLE IF EXISTS Articles;
+
+CREATE TABLE Articles (
+	Id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+	Title VARCHAR(50),
+	Catagories VARCHAR(200),
+	AuthorID INT,
+	FOREIGN KEY (AuthorID) REFERENCES Users(Uid),
+	Hits INT,
+	ImageFilePath VARCHAR(200),
+	ArticleText MEDIUMTEXT,
+	Response INT UNSIGNED,
+	FOREIGN KEY (Response) REFERENCES Articles(Id)
+);
